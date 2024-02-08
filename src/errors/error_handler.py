fa9f1bc1a2ce33a0a76dd67e7587a4f7821365e8
Willian <willian20091@gmail.com>
2024-02-08 15:25:17 -0300

    Feet: Impementing validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,11 +1,24 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntity
 
 def handle_errors(error: Exception) -> HttpResponse:
+
+    if isinstance(error, HttpUnprocessableEntity):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "Errors": [{
+                    "Title": error.name,
+                    "Detail": error.message
+                }]
+            }
+        )
+
     return HttpResponse(
         status_code=500,
         body={
             "Errors": [{
-                "Totle": "Server error",
+                "Title": "Server error",
                 "Detail": str(error)
             }]
         }
---
+++ src/errors/error_types/http_unprocessable_entity.py
@@ -0,0 +1,7 @@
+class HttpUnprocessableEntity(Exception):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
+        self.name = "unprocessable Entity"
+        self.status_code = 422
